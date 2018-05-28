76718fef5a71004cdb4eb816cbb69cd30b22530b
Robin <the.one.and.only.inno@gmail.com>
2018-05-28 20:34:06 +0200

    Removed some Errors

--- PanzerSpielV2/GameObjectFactory.cpp
+++ PanzerSpielV2/GameObjectFactory.cpp
@@ -2,8 +2,13 @@
 
 const static size_t max_gameobjects = 100;
 
+
+std::vector<GameObject> GameObjectFactory::m_GameObjects;
+
 bool GameObjectFactory::Init()
 {
 	m_GameObjects.reserve(max_gameobjects);
+
+
 	return false;
 }
--- PanzerSpielV2/Main.cpp
+++ PanzerSpielV2/Main.cpp
@@ -1,9 +1,6 @@
 #include <Windows.h> 
 #include "Engine.h"
 
-#include "Component.h"
-#include "InputComponent.h"
-#include "ComponentManager.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
