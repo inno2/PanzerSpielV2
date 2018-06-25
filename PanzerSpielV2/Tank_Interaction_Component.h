ec5a08438861356963227d7e15064c3e76d13228
Robin <the.one.and.only.inno@gmail.com>
2018-06-25 21:39:30 +0200

    fixes 2

--- PanzerSpielV2/Tank_Interaction_Component.h
+++ PanzerSpielV2/Tank_Interaction_Component.h
@@ -3,12 +3,7 @@
 
 class Tank_Interaction_Component : public Component
 {
-<<<<<<< HEAD
 public:
 	float accelerate;
 	float rotate;
-=======
-public: float accelerate;
-public: float rotate;
->>>>>>> 71fa6a4eeab0016acd3c421dcca4dc9fdf86ffa9
 };
